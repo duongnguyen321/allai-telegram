b29364663e7dc22a010a2f342260c6e2d578c119
Nguyen Minh Nhat Duong <duongcoilc2004@gmail.com>
2023-04-04 11:47:37 +0700

    update code

--- src/instance/instance.js
+++ src/instance/instance.js
@@ -1,6 +1,9 @@
 const e = require("node-telegram-bot-api"),
   { Configuration: n, OpenAIApi: o } = require("openai");
 require("dotenv").config();
-const i = new e(process.env.TOKEN, { polling: !0 }),
+const i = new e(process.env.TOKEN, {
+    polling: !0,
+    webHook: { port: process.env.PORT, autoOpen: true },
+  }),
   r = new o(new n({ apiKey: process.env.OPENAI_API_KEY }));
 module.exports = { bot: i, openai: r };
