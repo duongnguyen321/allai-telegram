3d3097879b28d6cad6abfcfe33334613bb8e5be0
Nguyen Minh Nhat Duong <duongcoilc2004@gmail.com>
2023-04-06 16:51:37 +0700

    update code

--- src/instance/instance.js
+++ src/instance/instance.js
@@ -2,11 +2,11 @@ const TelegramBot = require("node-telegram-bot-api");
 const { Configuration, OpenAIApi } = require("openai");
 require("dotenv").config();
 // Initialize Telegram bot with token
-const telegramBot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {
   polling: true,
 });
 // Initialize OpenAI API with API key
 const openaiApi = new OpenAIApi(
   new Configuration({ apiKey: process.env.OPENAI_API_KEY })
 );
-module.exports = { telegramBot, openaiApi };
+module.exports = { bot, openaiApi };
